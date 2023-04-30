9730944cda12fbab627e175cdaafbcbbc4f3f1a5
meilani <lauraho2006@gmail.com>
2023-04-30 02:22:40 -0400

    desktop style

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,27 @@
+import heroImgDesktop from '../images/image-hero-desktop.webp'
+import heroImgTablet from '../images/image-hero-tablet.webp'
+import heroImgMobile from '../images/image-hero-mobile.webp'
 import Button from './Button'
 
 const HeroSection = () => {
     return (
         <section className="hero">
-            <div className="col col-1">
-                <h2>
-                    Maximize skill,<br />
-                    minimize budget 
-                </h2>
-                <p>
-                    Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.
-                </p>
-                <Button href="#" txt="Get Started" />
-            </div>
-            <div className="col col-2">
-                <div className="stat stat-1">
-                    <p className="stat-name">Course hours</p>
-                    <p className="stat-num">1451</p>
+            <div className="container">
+                <div className="col col-1">
+                    <h2>
+                        Maximize skill,<br />
+                        minimize budget 
+                    </h2>
+                    <p>
+                        Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.
+                    </p>
+                    <Button href="#" txt="Get Started" />
                 </div>
-                <div className="stat stat-2">
-                    <p className="stat-name">Members</p>
-                    <p className="stat-num">29k</p>
+                <div className="col col-2">
+                    <img 
+                        src={heroImgDesktop}
+                        alt=""
+                    />
                 </div>
             </div>
         </section>
